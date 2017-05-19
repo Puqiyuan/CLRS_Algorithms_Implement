61e4e491742f2ade4136ac86660ae2580f8295fc
Qiyuan Pu <pqy7172@gmail.com>
2017-05-19 11:26:15 +0800

    test stack operation such as pop, push

--- chapter10/lib/operation.c
+++ chapter10/lib/operation.c
@@ -24,3 +24,30 @@ bool stack_empty(Stack S)
 
   return false;	 
 }
+
+
+int pop(Stack *S)
+{
+  if (stack_empty(*S))
+    return 0;
+
+  else
+    S->top = S->top - 1;
+  
+  return S->stack[S->top + 1];
+}
+
+
+int disply(Stack S)
+{
+  int i;
+
+  printf("The top of the stack: %d\n", S.top);
+  
+  for (i = S.top; i >= 1; i--)
+    printf("%d ", S.stack[i]);
+
+  printf("\n");
+
+  return 0;
+}
--- chapter10/main.c
+++ chapter10/main.c
@@ -1,3 +1,21 @@
+/*
+  Test result:
+  pqy@sda1:~/.../chapter10$ gcc main.c                                                                    
+  pqy@sda1:~/.../chapter10$ ./a.out 
+  Test for stack_empty function: 1 the value of S.top: 0
+  Push 34, 100, 199:
+  Display: 
+  The top of the stack: 3
+  199 100 34 
+  pop one element:
+  The return element of pop: 199
+  Display after pop:
+  The top of the stack: 2
+  100 34 
+  pqy@sda1:~/.../chapter10$ 
+  [pqy@sda1:0]  0:bash                                                       1.27 82% Fri 2017-05-19 11:24
+ */
+
 #include "stack.h"
 #include "./lib/operation.c"
 
@@ -8,7 +26,21 @@ int main(int argc, char *argv[])
   init_stack(&S);
 
   printf("Test for stack_empty function: %d the value of S.top: %d\n", stack_empty(S), S.top);
-  
+
+  printf("Push 34, 100, 199:\n");
+  push(&S, 34);
+  push(&S, 100);
+  push(&S, 199);
+
+  printf("Display: \n");
+  disply(S);
+
+  printf("pop one element:\n");
+  int x = pop(&S);
+  printf("The return element of pop: %d\n", x);
+  printf("Display after pop:\n");
+  disply(S);
+    
   return 0;
 }
 
